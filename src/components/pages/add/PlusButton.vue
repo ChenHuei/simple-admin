0346fce488d60a140be2d9f0077a52a3965d965d
leo <leo.chen@cloud-interactive.com>
2021-05-21 15:20:31 +0800

    Add plus button component

---
+++ src/components/pages/add/PlusButton.vue
@@ -0,0 +1,37 @@
+<template>
+  <div
+    class="
+      absolute
+      top-1/2
+      right-1/2
+      transform
+      -translate-x-1/2 -translate-y-1/2
+    "
+  >
+    <div
+      class="
+        w-20
+        h-20
+        flex
+        justify-center
+        items-center
+        bg-green-500
+        text-white text-2xl
+        rounded-full
+        cursor-pointer
+        transition
+        hover:opacity-60
+      "
+    >
+      +
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "PlusButton",
+});
+</script>
