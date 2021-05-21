23b08fd1c5f712d1715764dcacbb400f1b555feb
leo <leo.chen@cloud-interactive.com>
2021-05-21 15:20:40 +0800

    Add button component

---
+++ src/components/basic/Button.vue
@@ -0,0 +1,27 @@
+<template>
+  <button
+    class="
+      px-4
+      py-2
+      border
+      rounded-xl
+      transition
+      hover:bg-green-200
+      hover:border-green-200
+    "
+    style="outline: none"
+    v-on="$listeners"
+    v-bind="$attrs"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "BasicButton",
+  inheritAttrs: false,
+});
+</script>
